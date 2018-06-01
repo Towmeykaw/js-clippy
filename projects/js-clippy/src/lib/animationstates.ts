ea270a1853dd5825ac7cc2096a5daac5b11fcd6d
Towmeykaw <tkindmark@gmail.com>
2018-06-02 00:09:23 +0200

    Moved around files

---
+++ projects/js-clippy/src/lib/animationstates.ts
@@ -0,0 +1,3 @@
+export class AnimationStates {
+    public static State = { WAITING: 1, EXITED: 0 };
+}
---
+++ projects/js-clippy/src/lib/js-clippy.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgentComponent } from './agent.component';
+import { BalloonComponent } from './balloon.component';
+import { AngularDraggableModule } from 'angular2-draggable';
+
+@NgModule({
+  imports: [CommonModule,AngularDraggableModule],
+  declarations: [AgentComponent, BalloonComponent]
+  ,providers: [],
+  exports: [AgentComponent,BalloonComponent]
+})
+export class JsClippyModule { }
+  
