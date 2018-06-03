ebac5dddeeaa007545c4d7b76b2f22d7ae0f5e51
Towmeykaw <tkindmark@gmail.com>
2018-06-03 20:26:22 +0200

    Updated demo app and bumped npm packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgentComponent, BalloonComponent } from 'projects/js-clippy/src/public_api';
+import { AngularDraggableModule } from 'angular2-draggable';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgentComponent,
+    BalloonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularDraggableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
